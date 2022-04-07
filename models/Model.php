9a5f55b031234a7b955b43e88d51ede71334e4e2
EC2 Default User <ec2-user@ip-172-31-12-137.ap-northeast-1.compute.internal>
2022-04-07 12:00:55 +0900

    Heroku用に設定変更

--- models/Model.php
+++ models/Model.php
@@ -12,7 +12,8 @@
                     PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8',   //MySQL サーバーへの接続時に実行するコマンド
                 );
                 // データベースを扱う万能の神様誕生
-                $pdo = new PDO('mysql:host=localhost;dbname=facebook', 'root', '', $options);
+                // $pdo = new PDO('mysql:host=localhost;dbname=facebook', 'root', '', $options);
+                $pdo = new PDO('mysql:host=us-cdbr-east-05.cleardb.net;dbname=heroku_6e93e6768c08aea', 'b66150a69f9e1a', '48fe8fd1', $options);
                 return $pdo;
                 
             } catch (PDOException $e) {
